f9e4af317bbc3b77fffec1191f4f424fda08407d
Keith Meyer <meyer.keith@gmail.com>
2022-07-03 22:02:14 -0400

    Some basic API

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask , redirect , url_for , request 
+
+app = Flask(__name__)
+
+@app.route('/success/<name>')
+def success(name):
+    return 'welcome %s' % name
+
+@app.route('/login', methods = ['GET','POST']) 
+def login():
+    if request.method == 'POST':
+       user = request.form['daelious']
+       return redirect(url_for('success', name = user)) 
+    else:
+       return "INVALID"
+  
+if __name__ == '__main__':
+   app.run(debug = True)
